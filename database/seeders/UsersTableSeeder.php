363ecd40b92bdbf65eb9f0aac5434e7f62214dc2
mirkop <mirko85.dev@gmail.com>
2025-04-30 15:00:16 +0200

    add users seeder

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+
+    /**
+     * Auto generated seed file
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+
+        DB::table('users')->delete();
+
+        DB::table('users')->insert(array (
+            0 =>
+            array (
+                'id' => 1,
+                'name' => 'superadmin',
+                'email' => 'mirkopas85@gmail.com',
+                'email_verified_at' => NULL,
+                'password' => '$2y$12$QMtr51u0VsSyH3ha9tBt1u6D0PTR890.bYlg2ph7l3CuHKgzHzbLO',
+                'remember_token' => NULL,
+                'created_at' => '2025-04-27 08:37:11',
+                'updated_at' => '2025-04-27 08:37:11',
+            ),
+            1 =>
+            array (
+                'id' => 2,
+                'name' => 'michele',
+                'email' => 'michele@gmail.com',
+                'email_verified_at' => NULL,
+                'password' => '$2y$12$gKo.OiTkDyUecSDnCJv1KexslrROThR53hOjaHz1wCGjGWTaW5bUC',
+                'remember_token' => NULL,
+                'created_at' => '2025-04-30 12:58:35',
+                'updated_at' => '2025-04-30 12:58:35',
+            ),
+        ));
+
+
+    }
+}
