e5bd16785a935b7f9fb82ff730d9899857868ee6
mirkop <mirko85.dev@gmail.com>
2025-05-05 20:15:58 +0200

    update company model

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -7,6 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Company extends Model
 {
     //
+    protected $fillable = [
+        'name',
+        'vat_number',
+        'address',
+        'city_code',
+        'is_active'
+    ];
 
     public function users(){
         return $this->belongsToMany(User::class);
