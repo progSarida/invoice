6c8aa16f3b88cb9d054cc45916a5f6e41f1fa44d
progSarida <programmazione@sarida.it>
2025-05-20 14:22:46 +0200

    Fatta stampa pdf lista clienti (con filtri)

--- app/Filament/Company/Resources/ClientResource/Pages/ListClients.php
+++ app/Filament/Company/Resources/ClientResource/Pages/ListClients.php
@@ -5,6 +5,12 @@ namespace App\Filament\Company\Resources\ClientResource\Pages;
 use App\Filament\Company\Resources\ClientResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Barryvdh\DomPDF\Facade\Pdf;
+use Filament\Notifications\Notification;
+use Filament\Support\Enums\MaxWidth;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Blade;
 
 class ListClients extends ListRecords
 {
@@ -13,7 +19,39 @@ class ListClients extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make()->keyBindings(['f6']),
+            Actions\CreateAction::make()
+                ->icon('heroicon-o-plus-circle')
+                ->keyBindings(['f6']),
+            Actions\Action::make('stampa')
+                ->icon('heroicon-o-printer')
+                ->label('Stampa')
+                ->tooltip('Stampa elenco clienti')
+                // ->iconButton()                                                                  // mostro solo icona
+                ->color('primary')
+                ->action(function ($livewire) {
+                    $records = $livewire->getFilteredTableQuery()->get();                       // recupero risultato della query
+                    $filters = $livewire->tableFilters ?? [];                                   // recupero i filtri
+                    $search = $livewire->tableSearch ?? null;                                   // recupero la ricerca
+
+                    return response()
+                        ->streamDownload(function () use ($records, $search, $filters) {
+                            echo Pdf::loadHTML(
+                                Blade::render('pdf.clients', [
+                                    'clients' => $records,
+                                    'search' => $search,
+                                    'filters' => $filters,
+                                ])
+                            )
+                                ->setPaper('A4', 'landscape')
+                                ->stream();
+                        }, 'Clienti.pdf');
+
+                    Notification::make()
+                        ->title('Stampa avviata')
+                        ->success()
+                        ->send();
+                })
+                ,
         ];
     }
 }
---
+++ resources/views/pdf/clients.blade.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        body {
+            font-family: DejaVu Sans, sans-serif;
+            font-size: 12px;
+        }
+
+        table {
+            border-collapse: collapse;
+            width: 100%;
+            border: 2px solid black;                          /* bordi esterni */
+        }
+
+        th, td {
+            border-top: 1px dashed black;                     /* tratteggio tra le righe */
+            border-bottom: 1px dashed black;                  /* tratteggio tra le righe */
+            border-left: none;                                  /* nessun bordo verticale */
+            border-right: none;                                 /* nessun bordo verticale */
+            padding: 4px;
+            text-align: left;
+        }
+
+        thead th {
+            border-top: 2px solid black;                      /* bordi intestazione sopra */
+            border-bottom: 2px solid black;                   /* bordi intestazione sotto */
+        }
+
+        tbody tr:last-child td {
+            border-bottom: 2px solid black;                   /* bordi esterni sotto */
+        }
+
+        tr td:first-child,
+        tr th:first-child {
+            border-left: 2px solid black;                     /* bordi esterni sinistra */
+        }
+
+        tr td:last-child,
+        tr th:last-child {
+            border-right: 2px solid black;                    /* bordi esterni destra */
+        }
+    </style>
+</head>
+<body>
+    <h2><u>Elenco Clienti</u></h2>
+    @if(!empty($filters))
+        <p><strong>Filtri applicati:</strong></p>
+        <ul>
+            @if($search)
+                <li>
+                    Ricerca:
+                    {{ $search }}
+                </li>
+            @endif
+            @foreach($filters as $field => $data)
+                @if(!empty($data['values']))
+                    <li>
+                        {{ ucfirst($field) == 'Type' ? 'Tipo' : ''}}:
+                        {{ implode(', ', array_map(fn($value) => \App\Enums\ClientType::tryFrom($value)?->getLabel(), $data['values'])) }}
+                    </li>
+                @endif
+            @endforeach
+        </ul>
+    @endif
+    <table>
+        <thead>
+            <tr>
+                <th>Tipo</th>
+                <th>Denominazione</th>
+                <th>Email</th>
+                <th>Codice univoco</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($clients as $client)
+                <tr>
+                    <td>{{ $client->type->getLabel() }}</td>
+                    <td>{{ trim($client->denomination) }}</td>
+                    <td>{{ $client->email }}</td>
+                    <td>{{ $client->ipa_code }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</body>
+</html>
