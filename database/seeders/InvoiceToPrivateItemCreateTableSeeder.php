c34041d7b5566f88907627555c8d8c565c060e70
progSarida <programmazione@sarida.it>
2025-12-18 12:08:59 +0100

    Versione definitiva del seeder per la creazione delle voci delle fatture a privati

--- database/seeders/InvoiceToPrivateItemCreateTableSeeder.php
+++ database/seeders/InvoiceToPrivateItemCreateTableSeeder.php
@@ -21,41 +21,46 @@ class InvoiceToPrivateItemCreateTableSeeder extends Seeder
         Log::info('Inizio creazione voci fatture con IVA a privati.');
         $i = 0;
         try{
-            // $invoices = Invoice::whereNull('contract_id')->get();                                   // tutte le fatture senza contratto (verso privati)
+            $invoices = Invoice::whereNull('contract_id')->get();                                   // tutte le fatture senza contratto (verso privati)
             // $invoices = Invoice::whereNull('contract_id')->where('vat', '!=', 0)->get();            // fatture senza contratto (verso privati) con iva
-            $invoices = Invoice::where('id', 5197)->get();                                          // fattura di test
+            // $invoices = Invoice::where('id', 2606)->get();                                          // fattura di test
+            // $invoices = Invoice::where('id', 5197)->get();                                          // fattura di test
+            // $invoices = Invoice::where('id', 6241)->get();                                          // fattura di test
+            // $invoices = Invoice::whereIn('id', [2606, 5197, 6241])->get();                          // fatture di test
             Log::info('Numero di fatture: ' . count($invoices));
             foreach($invoices as $invoice){
                 $i++;
+
                 Log::info($i . ') Creazione voci fattura (' . $invoice->id . ') ' . $invoice->getNewInvoiceNumber() . ' del ' . \Carbon\Carbon::parse($invoice->invoice_date)->format('d/m/Y'));
+
                 $aV = [
                     'invoice_id' => $invoice->id,
                     'invoice_element_id' => null,
                     'description' => 'Importo',
                     'amount' => (string) $invoice->importo,
                     'vat_code_type' => 'vc01',
-                    'total' => (string) $invoice->importo * 1.22,
+                    'total' => $invoice->importo * 1.22,
                 ];
                 $itemV = InvoiceItem::create($aV);
                 $itemV->calculateTotal();
                 $itemV->save();
                 $itemV->autoInsert();
                 Log::info('Creata voce importo');
-                if($invoice->spese > 0){
-                    $aE = [
-                        'invoice_id' => $invoice->id,
-                        'invoice_element_id' => 8,
-                        'description' => 'Spese',
-                        'amount' => $invoice->spese,
-                        'vat_code_type' => 'vc06',
-                        'total' => $invoice->spese,
-                    ];
-                    $itemE = InvoiceItem::create($aE);
-                    $itemE->calculateTotal();
-                    $itemE->save();
-                    $itemE->autoInsert();
-                    Log::info('Creata voce spese');
-                }
+
+                $aE = [
+                    'invoice_id' => $invoice->id,
+                    'invoice_element_id' => 8,
+                    'description' => 'Spese',
+                    'amount' => $invoice->spese,
+                    'vat_code_type' => 'vc01',
+                    'total' => $invoice->spese * 1.22,
+                ];
+                $itemE = InvoiceItem::create($aE);
+                $itemE->calculateTotal();
+                $itemE->save();
+                $itemE->autoInsert();
+                Log::info('Creata voce spese');
+
                 if($invoice->ordinario > 0){
                     $aO = [
                         'invoice_id' => $invoice->id,
@@ -71,6 +76,7 @@ class InvoiceToPrivateItemCreateTableSeeder extends Seeder
                     $itemO->autoInsert();
                     Log::info('Creata voce importo esente iva');
                 }
+
                 if($invoice->temporaneo > 0){
                     $aT = [
                         'invoice_id' => $invoice->id,
@@ -86,6 +92,7 @@ class InvoiceToPrivateItemCreateTableSeeder extends Seeder
                     $itemT->autoInsert();
                     Log::info('Creata voce canone');
                 }
+
                 if($invoice->rimborsi > 0){
                     $aT = [
                         'invoice_id' => $invoice->id,
@@ -101,6 +108,7 @@ class InvoiceToPrivateItemCreateTableSeeder extends Seeder
                     $itemT->autoInsert();
                     Log::info('Creata voce rimborso');
                 }
+
                 if($invoice->affissioni > 0){
                     $aT = [
                         'invoice_id' => $invoice->id,
@@ -116,6 +124,7 @@ class InvoiceToPrivateItemCreateTableSeeder extends Seeder
                     $itemT->autoInsert();
                     Log::info('Creata voce cauzione');
                 }
+
                 if($invoice->bollo > 0){
                     $aS = [
                         'invoice_id' => $invoice->id,
